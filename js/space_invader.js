61bd5b2d6c45486721c1534f26749c467784897e
Luis <luismetzger@me.com>
2016-06-27 12:44:50 -0600

    Initial commit. I have setup the basic foundation of my game including directories, html div where my game will live in the markup and now setting up code block for my JS.

---
+++ js/space_invader.js
@@ -0,0 +1,12 @@
+function SpaceInvader() {
+  //ATD object variables
+
+}
+SpaceInvader.prototype = {
+  start: function() {
+    //Lets start the game
+  }
+}
+
+var space_invader = new SpaceInvader();
+space_invader.start();
