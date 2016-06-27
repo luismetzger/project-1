851302902c976918b3f8f70765ca629869d20d44
Luis <luismetzger@me.com>
2016-06-27 13:01:35 -0600

    Tryingt o setup my main ATD and protoype for the main functions of the game. Still only have the foundation, no DOM manipulation logic just yet.

--- js/space_invader.js
+++ js/space_invader.js
@@ -1,10 +1,32 @@
 function SpaceInvader() {
   //ATD object variables
+   this.fps = 30;
+   this.canvas = null;
+   this.width = 0;
+   this.height = 0;
+   this.minVelocity = 15;
+   this.maxVelocity = 30;
+   this.stars = 100;
+   this.intervalId = 0;
 
 }
 SpaceInvader.prototype = {
+  initialize: function(div) {
+    var self= this;
+
+    //Storing the DIV container
+    this.containerDiv = div;
+    self.width = window.InnerWidth;
+    self.height = window.InnerHeight;
+  },
   start: function() {
     //Lets start the game
+  },
+  stop: function() {
+
+  },
+  pause: function() {
+
   }
 }
 
