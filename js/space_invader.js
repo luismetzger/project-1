667577ca5c49f067b44e6d863ea3fc500e7d2283
Luis <luismetzger@me.com>
2016-06-27 18:37:55 -0600

    Still working on my animated background. Not sure why my code is not working yet but I believe it has to do with this.fps ... Still trying to figure out why this method is causing the issue to not load.

--- js/space_invader.js
+++ js/space_invader.js
@@ -8,7 +8,6 @@ function SpaceInvader() {
    this.maxVelocity = 30;
    this.stars = 100;
    this.intervalId = 0;
-
 }
 SpaceInvader.prototype = {
   initialize: function(div) {
@@ -16,19 +15,90 @@ SpaceInvader.prototype = {
 
     //Storing the DIV container
     this.containerDiv = div;
-    self.width = window.InnerWidth;
-    self.height = window.InnerHeight;
+    self.width = window.innerWidth;
+    self.height = window.innerHeight;
+
+    window.addEventListener('resize', function resize(event) {
+      self.width = window.innerWidth;
+      self.height = window.innerHeight;
+      self.canvas.width = self.width;
+      self.canvas.width = self.height;
+      self.draw();
+    });
+
+    //Create the canvas
+    var canvas = document.createElement('canvas');
+    div.appendChild(canvas);
+    this.canvas = canvas;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
   },
   start: function() {
-    //Lets start the game
+    //Lets create some stars
+    var stars = [];
+    for(var i=0; i < this.stars;i++) {
+      stars[i] = new Star(Math.random()*this.width, Math.random()*this.height, Math.random()*3+1,
+		 (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+    }
+      this.stars = stars;
+
+      var self = this;
+      //Now let's start the timer
+      this.intervalId = setInterval(function() {
+    		self.update();
+    		self.draw();
+    	}, 1000 / this.fps);
   },
   stop: function() {
-
+    clearInterval(this.intervalId);
   },
-  pause: function() {
+  update: function() {
+    var passed_time = 1 / this.fps;
+    for(var i=0; i < this.stars.length;i++) {
+        var star = this.stars[i];
+        star.y += passed_time * star.velocity;
+        if(star.y > this.height) {
+          this.stars[i] = new Star(Math.random()*this.width, 0, Math.random()*3+1,
+		 	(Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+        }
+    }
+  },
+  draw: function() {
+      // Declare drawing context
+      var ctx = this.canvas.getContext('2d');
+      console.log(ctx);
 
+      //Let's fill-in the background of canvas
+      ctx.fillStyle = '#00000';
+      ctx.fillRect(0, 0 , this.width, this.height);
+
+      //Now let's get the stars to appear by drawing them in
+      ctx.fillStyle = '#ffffff';
+      for(var i=0; i < this.stars.length;i++) {
+        var star = this.stars[i];
+        ctx.fillRect(star.x, star.y, star.size, star.size);
+      }
+    }
   }
+
+function Star(x, y, size, velocity) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.velocity = velocity;
 }
 
+
+//Let's make the background animated!
+var container = document.getElementById('bg-container');
 var space_invader = new SpaceInvader();
+space_invader.initialize(container);
 space_invader.start();
+
+// function randomise() {
+// 	starfield.stop();
+// 	starfield.stars = Math.random()*1000 + 50;
+// 	starfield.minVelocity = Math.random()*30+5;
+// 	starfield.maxVelocity = Math.random()*50 + starfield.minVelocity;
+// 	starfield.start();
+// }
